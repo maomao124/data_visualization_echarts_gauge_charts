4c497ca9aa7321ebba58e6947efd724359b8b684
mao <1296193245@qq.com>
2023-06-15 14:25:22 +0800

    data_visualization_echarts_gauge_charts

---
+++ src/main/java/mao/data_visualization_echarts_gauge_charts/DataVisualizationEchartsGaugeChartsApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_gauge_charts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationEchartsGaugeChartsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationEchartsGaugeChartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_echarts_gauge_charts/DataVisualizationEchartsGaugeChartsApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_gauge_charts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationEchartsGaugeChartsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
